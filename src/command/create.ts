b50cc3ebbff017c9378fa6ce10e37e30d5f84cd2
lick <821067147@qq.com>
2026-03-29 14:38:38 +0800

    feat: 完善功能

--- src/command/create.ts
+++ src/command/create.ts
@@ -1,8 +1,12 @@
 
 import path from 'path';
 import fs from 'fs-extra'
+import {gt} from 'lodash'
+import axios, { AxiosResponse } from 'axios'
 import { input, select } from '@inquirer/prompts';
 import { clone } from '../utils/clone'
+import chalk from "chalk";
+import { version,name } from '../../package.json'
 export interface TemplateInfo {
   name: string; // 模板名称
   downloadUrl: string; // 模板下载地址
@@ -41,7 +45,37 @@ export function isOverwrite(fileName: string) {
     ],
   });
 }
-export async function create(projectName: string) {
+
+export const getNpmInfo = async (npmName: string) => {
+  const npmUrl = `https://registry.npmjs.org/${name}`;
+  let res = {}
+  try {
+    res = await axios.get(npmUrl)
+  } catch (error) {
+    console.error(error)
+  }
+  return res
+};
+export const getNpmLatestVersion = async (name: string) => {
+  const { data } = await getNpmInfo(name) as AxiosResponse
+  // console.info('npm info',data)
+  return data['dist-tags'].latest
+}
+// 检查版本是否更新
+export const checkVersion = async (name: string, version: string) => {
+  const latestVersion = await getNpmLatestVersion(name);
+  const need = gt(latestVersion, version);
+  if (need) {
+    console.warn(
+      `检查到mengbo最新版本： ${chalk.blackBright(latestVersion)}，当前版本是：${chalk.blackBright(version)}`
+    );
+    console.log(
+      `可使用： ${chalk.yellow('npm install @dream-mb/amber-cli@latest')}，或者使用：${chalk.yellow('mengbo update')}更新`
+    );
+  }
+  return need;
+}
+export async function create(projectName?: string) {
   // 初始化模板列表
   const templateList = Array.from(templates).map((item: [string, TemplateInfo]) => {
     const [name, info] = item;
@@ -64,16 +98,16 @@ export async function create(projectName: string) {
       return; // 不覆盖直接结束
     }
   }
+  // 检查版本更新
+  await checkVersion(name, version);
   const templateName = await select({
     message: '请选择模板',
     choices: templateList,
   });
   const info = templates.get(templateName);
-
+  console.log('info', info);
   if (info) {
     await clone(info.downloadUrl, projectName, ['-b', info.branch]);
   }
-  console.log('info', info);
   console.log('create', projectName)
-
 }
